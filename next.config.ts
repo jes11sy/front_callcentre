636ac926b5970c40c1930b8a5a4ae0b54bd4b21b
jes11sy <defouz03@gmail.com>
2025-12-18 18:54:14 +0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -158,7 +158,7 @@ const nextConfig: NextConfig = {
               isDevelopment 
                 ? "connect-src 'self' https: wss: ws: http://localhost:* ws://localhost:* http://127.0.0.1:* ws://127.0.0.1:*"
                 : "connect-src 'self' https://api.lead-schem.ru wss://api.lead-schem.ru https://api.test-shem.ru wss://api.test-shem.ru https://s3.twcstorage.ru",
-              "media-src 'self' blob: data: https://s3.twcstorage.ru",
+              "media-src 'self' blob: data: https://s3.twcstorage.ru https://s3.timeweb.com",
               "object-src 'none'",
               "base-uri 'self'",
               "form-action 'self'",

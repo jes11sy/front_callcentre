c949c5078006546835ef80645e82debff1934d1c
jes11sy <defouz03@gmail.com>
2026-02-07 20:45:45 +0400

    1

--- src/components/orders/OrderEditModal.tsx
+++ src/components/orders/OrderEditModal.tsx
@@ -82,13 +82,13 @@ export const OrderEditModal = ({
 
   return (
     <div 
-      className="fixed left-0 right-0 bottom-0 top-[64px] z-[9997] lg:top-0 lg:flex lg:items-center lg:justify-center lg:bg-black/50 lg:p-4"
+      className="fixed inset-0 z-[9997] flex items-end lg:items-center lg:justify-center bg-black/50 lg:p-4"
       onClick={onClose}
     >
       <div 
-        className={`absolute inset-0 lg:static lg:w-auto lg:h-auto lg:max-h-[85vh] lg:rounded-lg overflow-hidden flex flex-col ${isV2 
-          ? "bg-[#F3F3EE] dark:bg-[#1e2530] shadow-xl dark:shadow-none lg:max-w-2xl lg:border border-gray-200 dark:border-gray-700 font-myriad"
-          : "bg-[#0f0f23] shadow-[0_0_30px_rgba(255,215,0,0.2)] lg:max-w-2xl lg:border-2 border-[#FFD700]/50"
+        className={`w-full h-[calc(100vh-64px)] lg:h-auto lg:w-[672px] lg:max-h-[85vh] lg:rounded-lg overflow-hidden flex flex-col ${isV2 
+          ? "bg-[#F3F3EE] dark:bg-[#1e2530] shadow-xl dark:shadow-none lg:border border-gray-200 dark:border-gray-700 font-myriad"
+          : "bg-[#0f0f23] shadow-[0_0_30px_rgba(255,215,0,0.2)] lg:border-2 border-[#FFD700]/50"
         }`}
         onClick={(e) => e.stopPropagation()}
       >

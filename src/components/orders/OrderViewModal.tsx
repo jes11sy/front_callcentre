f3be946a7eb9c4f2a3d6e2f1364106c0e9eec861
jes11sy <defouz03@gmail.com>
2026-02-07 19:38:52 +0400

    Update OrderViewModal.tsx

--- src/components/orders/OrderViewModal.tsx
+++ src/components/orders/OrderViewModal.tsx
@@ -89,7 +89,7 @@ const OrderViewModalComponent = ({
 
   return (
     <div 
-      className="fixed inset-0 z-[10000] flex items-start sm:items-center justify-center bg-black/50 sm:p-4"
+      className="fixed inset-0 z-[10000] sm:flex sm:items-center sm:justify-center sm:bg-black/50 sm:p-4"
       onClick={handleClose}
     >
       <div 
